9b03166f8d6c3a39c8d5554367d9ee9fe60a7f50
anne belka and skye atwood <skye@>
2016-10-25 09:48:32 -0700

    complete working loop with number variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,19 @@
-var addByMultiples = function(inputNumber1, inputNumber2) {
+var addByMultiples = function(number1, number2) {
 
   var total=0;
 
-  for(var index=0; index < inputNumber1; index += inputNumber2){
+  for(var index=0; index < number1; index += number2){
     total += number2;
 
+    alert(total);
   }
-  alert(total);
 }
 
 $(document).ready(function() {
   $("form").submit(function(event){
-    var inputNumber1 = $("input#number1").val();
-    var inputNumber2 = $("input#number2").val();
+    var inputNumber1 = parseInt($("input#number1").val());
+    var inputNumber2 = parseInt($("input#number2").val());
+    console.log(inputNumber1);
 
     addByMultiples(inputNumber1,inputNumber2);
 
